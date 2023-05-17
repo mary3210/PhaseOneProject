c7f788aa9862c1ecdb6a701c24172d13f3b90304
mary3210 <maryrodri3210@gmail.com>
2023-05-17 15:54:38 -0500

    add date change

--- src/projects/01-signature/01-signature-app.jsx
+++ src/projects/01-signature/01-signature-app.jsx
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import Title from "../components/Title";
 
 export default function EsignatureApp() {
-
-    const [name, setName] = useState("");
+    const [name, setName] = useState("Name");
+    const [date, setDate] = useState("");
     const handleNameChange = (e) => {
         // console.log(e.target.value)
         setName(e.target.value)
     };
+    const handleDateChange = (e) 
     const inputStyle = {
         border: "none",
         borderBottom: "2px, dotted",
@@ -18,11 +19,11 @@ export default function EsignatureApp() {
     return (
         <div className='container text-center'>
         <Title classes={"subtitles"} text={name}/>
-        <Title classes={"main-title"} text={"Date"}/>
+        <Title classes={"main-title"} text={!date ? "04/12/2023" : date}/>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore veritatis odit eum ea magnam nemo asperiores inventore laudantium. Modi repellat eius labore fugit non numquam nihil in ratione molestiae et!</p>
        <footer className="d-flex" style={{justifyContent: "space-around", position: "relative",
     top:"40vh"}}>
-        <input type="date" value={""} style={inputStyle}/>
+        <input type="date" value={date} style={inputStyle} onChange={handleDateChange}/>
         <input type="text" value={name} style={inputStyle} onChange={handleNameChange}/>
 
        </footer>
