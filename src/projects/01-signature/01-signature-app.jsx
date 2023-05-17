97268cd5998d35a07a6577d97343d5bacea8ffa7
mary3210 <maryrodri3210@gmail.com>
2023-05-16 19:46:02 -0500

    Add additional styling to the footer in 01-sig.app

--- src/projects/01-signature/01-signature-app.jsx
+++ src/projects/01-signature/01-signature-app.jsx
@@ -3,16 +3,22 @@ import Title from "../components/Title";
 export default function EsignatureApp() {
     const inputStyle = {
         border: "none",
-        borderBottom: "2px, dotted"
+        borderBottom: "2px, dotted",
+        outline: "none",
+        padding: ".35rem 0",
     };
+    document.body.style.background= "#eee";
     return (
         <div className='container text-center'>
         <Title classes={"subtitles"} text={"Name"}/>
         <Title classes={"main-title"} text={"Date"}/>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore veritatis odit eum ea magnam nemo asperiores inventore laudantium. Modi repellat eius labore fugit non numquam nihil in ratione molestiae et!</p>
-       
+       <footer className="d-flex" style={{justifyContent: "space-around", position: "relative",
+    top:"40vh"}}>
         <input type="date" value={""} style={inputStyle}/>
         <input type="text" value={""} style={inputStyle}/>
+
+       </footer>
         </div>
     )
 }
