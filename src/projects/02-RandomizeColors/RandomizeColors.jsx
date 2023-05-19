2960d1777f43c8275d5d87c6945f5de6a280c3a2
mary3210 <maryrodri3210@gmail.com>
2023-05-18 23:28:35 -0500

    Add HandleClick function in RandomizeColors component

--- src/projects/02-RandomizeColors/RandomizeColors.jsx
+++ src/projects/02-RandomizeColors/RandomizeColors.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Title from "../components/Title";
 export default function RandomizeColors() {
+    function HandleClick () {
+        console.log('click')
+    }
   return (
     <div className="container m-auto text-center">
       <Title text={"Randomize Color"} classes={"mb-4"} />
-      <button className="btn btn-danger">Click me</button>
+      <button className="btn btn-danger" onClick={HandleClick}>Click me</button>
       <button className="btn btn-success">Click me</button>
       <button className="btn btn-primary">Click me</button>
       <button className="btn btn-warning">Click me</button>
