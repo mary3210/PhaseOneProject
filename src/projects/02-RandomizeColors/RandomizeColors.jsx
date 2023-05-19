6a9761eadcc1c1c788810651437c195f462bee3e
mary3210 <maryrodri3210@gmail.com>
2023-05-19 00:17:45 -0500

    Create getRandomColor function

--- src/projects/02-RandomizeColors/RandomizeColors.jsx
+++ src/projects/02-RandomizeColors/RandomizeColors.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import Title from "../components/Title";
 export default function RandomizeColors() {
-    function HandleClick () {
-        console.log('click')
+    function HandleClick (event) {
+        console.log(event.target)
+    }
+    const handleSecClick = (e) => {
+
+        console.log(e.target);
+    }
+    function getRandomColor () {
+        let letters = "0123456789ABCDEF";
+        let color = "#"
+        for (let i=0; i<6; i++){
+            color+= letters[Math.floor(Math.random() * 16)]
+        }
+        return color
     }
   return (
     <div className="container m-auto text-center">
       <Title text={"Randomize Color"} classes={"mb-4"} />
-      <button className="btn btn-danger" onClick={HandleClick}>Click me</button>
-      <button className="btn btn-success">Click me</button>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-warning">Click me</button>
+      <button className="btn btn-danger" onClick={(event) => HandleClick(event)}>Click me</button>
+      <button className="btn btn-success" onClick={handleSecClick}>Click me</button>
+      <button className="btn btn-primary" onClick={handleSecClick}>Click me</button>
+      <button className="btn btn-warning" onClick={handleSecClick}>Click me</button>
     </div>
   );
 }
