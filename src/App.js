8dd47571a36dc5349afbbf765ecc8ea0fbcf429a
mary3210 <maryrodri3210@gmail.com>
2023-05-18 22:50:37 -0500

    imported RandomizeColors component in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import RandomizeColors from './projects/02-RandomizeColors/RandomizeColors';
 // import EsignatureApp from './projects/01-signature/01-signature-app';
 
 function App() {
   return (
     <div className="App">
      {/* <EsignatureApp/> */}
+     <RandomizeColors/>
     </div>
   );
 }
