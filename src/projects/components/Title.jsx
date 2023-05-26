282cabd8f4c199c9449883b779c61497e49e3dc2
mary3210 <maryrodri3210@gmail.com>
2023-05-25 21:58:39 -0500

    Updated Title.jsx

--- src/projects/components/Title.jsx
+++ src/projects/components/Title.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Title({classes, text}) {
   return (
-      <h1 className={!classes ? "title" : classes && "title text-center"}>{!text ? "Title" :text}</h1> 
+      <h1 className={!classes ? "title" : classes || "title text-center"}>{!text ? "Title" :text}</h1> 
   )
 }
