135df698ba76a724e108ea25a6c7243b578f0e4a
mary3210 <maryrodri3210@gmail.com>
2023-05-25 21:43:19 -0500

    imported LikePhotoApp in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import LikePhotoApp from './projects/03-like-my-photo/LikePhotoApp';
 import RandomizeColors from './projects/02-RandomizeColors/RandomizeColors';
 // import EsignatureApp from './projects/01-signature/01-signature-app';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="App">
      {/* <EsignatureApp/> */}
-     <RandomizeColors/>
+     {/* <RandomizeColors/> */}
+     <LikePhotoApp/>
     </div>
   );
 }
